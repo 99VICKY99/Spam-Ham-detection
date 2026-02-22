38778616b1721ef33b770ab02b638489ecffd72c
Vicky <vickyprasad2732003@gmail.com>
2026-02-22 10:22:39 +0530

    Update author information in setup.py and copyright details in HTML templates

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    author="Tanisha Verma",
-    author_email="vtanishaaa@gmail.com",
+    author="Vicky Prasad",
+    author_email="vickyprasad2732003@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
